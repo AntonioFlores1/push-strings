401b262f285fcb23c05fd6a67862ff80a7999f16
AntonioFlores1 <af513249@gmail.com>
2018-10-09 14:12:54 -0400

    Initial Commit

---
+++ Intro to strings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Intro to strings
+//
+//  Created by Pursuit on 10/9/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
